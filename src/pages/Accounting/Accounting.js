087ebedd5ed295ef18b2acc53a5e1b2fe1d9a670
lzy77_ <channelforletsplayshik@gmail.com>
2023-08-04 18:41:23 +0300

    Add get report functionality. Update links (.a) styles

--- src/pages/Accounting/Accounting.js
+++ src/pages/Accounting/Accounting.js
@@ -5,18 +5,37 @@ import { useEffect, useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 function Accounting() {
+  const [activeTab, setActiveTab] = useState(0);
+
   const navigate = useNavigate();
   useEffect(() => {
     navigate('./orders');
   }, []);
+
+  const getExcel = async () => {
+    await fetch('https://localhost:7134/api/MoneyTransfer/report');
+  };
 
   return (
     <div className={s.app}>
       <Header tabTitle={'Accounting'} />
       <div className={s.app__content}>
         <div className={s.app__content__navlinks}>
-          <NavLink to="./orders">Orders</NavLink>
-          <NavLink to="./transactions">transactions</NavLink>
+          <NavLink
+            to="./orders"
+            style={{ color: activeTab === 0 ? 'gray' : 'white' }}
+            onClick={() => setActiveTab(0)}
+          >
+            Orders
+          </NavLink>
+          <NavLink
+            to="./transactions"
+            style={{ color: activeTab === 1 ? 'gray' : 'white' }}
+            onClick={() => setActiveTab(1)}
+          >
+            Transactions
+          </NavLink>
+          <a href="https://localhost:7134/api/MoneyTransfer/report">Excel</a>
         </div>
         <Outlet></Outlet>
       </div>
