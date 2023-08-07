8f3c8f071409748d6fff79e63da346e8dedf3fff
lzy77_ <channelforletsplayshik@gmail.com>
2023-08-07 15:34:54 +0300

    Add functionality in storage page

---
+++ src/pages/Storage/OrderItem.js
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from 'react';
+import s from './Storage.module.scss';
+
+function OrderItem({ id, date, name, statusId, products, storageProducts }) {
+  const [isDetails, setIsDetails] = useState(false);
+  const [_statusId, _setStatusId] = useState(statusId);
+
+  let status;
+  let isAllEnough = false;
+
+  switch (_statusId) {
+    case 0:
+      status = 'Waiting confirmation';
+      break;
+    case 1:
+      status = 'Waiting bill';
+      break;
+    case 2:
+      status = 'Waiting payment';
+      break;
+    case 3:
+      status = 'Waiting storage confirmation';
+      break;
+    case 4:
+      status = 'In Delivery';
+      break;
+    case 5:
+      status = 'Delivered';
+      break;
+    default:
+      status = 'UNDEFINED';
+      break;
+  }
+
+  const handleConfirm = () => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        id: id,
+        statusId: 4,
+      }),
+    };
+
+    fetch('https://localhost:7134/api/order', requestOptions);
+
+    _setStatusId(4);
+  };
+
+  const canBeOrdered = (id, amount) => {
+    let productOnStorage = storageProducts.find((product) => product.id === id);
+    console.log(amount + 'stor: ' + productOnStorage.amount);
+    if (productOnStorage == null) {
+      isAllEnough = false;
+      return false;
+    } else if (productOnStorage.amount >= amount) {
+      isAllEnough = true;
+      return true;
+    } else {
+      isAllEnough = false;
+      return false;
+    }
+  };
+
+  return (
+    <div
+      className={s.order_info}
+      onMouseEnter={() => setIsDetails(true)}
+      onMouseLeave={() => setIsDetails(false)}
+      style={isDetails ? { height: '250px' } : { height: '50px' }}
+    >
+      <div className={s.order_info__header}>
+        <div className={`${s.cell} ${s.id}`}>{id}</div>
+        <div className={s.cell}>{name}</div>
+        <div className={s.cell}>{date.slice(0, 10)}</div>
+        <div className={s.cell}>{status}</div>
+      </div>
+
+      {isDetails ? (
+        <div className={s.order_info__details}>
+          <div className={s.order_info__details__products}>
+            {products.map((product) => (
+              <div className={s.order_info__details__product}>
+                <div className={`${s.cell} ${s.id}`}>{product.id}</div>
+                <div className={`${s.cell} ${s.prop}`}>{product.name}</div>
+                <div className={`${s.cell} ${s.prop}`}>{product.price}$</div>
+                <div className={`${s.cell} ${s.prop}`}>
+                  {product.amount} Items
+                </div>
+                <div
+                  className={s.prop}
+                  style={
+                    !canBeOrdered(product.id, product.amount)
+                      ? { color: 'red' }
+                      : { color: 'green' }
+                  }
+                >
+                  {canBeOrdered(product.id, product.amount)
+                    ? 'Can be ordered'
+                    : 'Not enough'}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className={s.order_info__details__submittedMoney}></div>
+          <div className={s.order_info__details__inputs}>
+            <button
+              type="button"
+              className={`${s.app__content__product__form__button_add} ${
+                !isAllEnough ? s.disabled : ''
+              }`}
+              onClick={() => handleConfirm()}
+            >
+              Confirm order
+            </button>
+          </div>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
+}
+
+export default OrderItem;
--- src/pages/Storage/Storage.js
+++ src/pages/Storage/Storage.js
@@ -1,12 +1,49 @@
 // import './Storage.scss';
 import Header from '../Header';
+import { useEffect, useState } from 'react';
+import s from './Storage.module.scss';
+import OrderItem from './OrderItem';
 
 function Storage() {
+  const [orders, setOrders] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+  const [storageProducts, setStorageProducts] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    let res = await fetch('https://localhost:7134/api/Order/3');
+    let response = await res.json();
+    setOrders(response);
+    res = await fetch('https://localhost:7134/api/Product');
+    response = await res.json();
+    setStorageProducts(response);
+    setLoading(false);
+  };
+
   return (
-    <div className="app">
+    <div className={s.app}>
       <Header tabTitle={'Storage'} />
-      <div className="app__content">
-        <h1>Storage</h1>
+      <div className={s.app__content}>
+        {!isLoading ? (
+          orders.map((el, index) => (
+            <li key={el.id}>
+              <OrderItem
+                id={el.id}
+                date={el.submittedDateTime}
+                name={el.customerName}
+                statusId={el.orderStatusId}
+                products={el.orderedProducts}
+                storageProducts={storageProducts}
+              />
+            </li>
+          ))
+        ) : (
+          <h2>Loading...</h2>
+        )}
       </div>
     </div>
   );
